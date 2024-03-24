3851c3b1c3d68f1ea86081f471e580de36b65978
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-03-24 14:12:07 +0000

    Add screenshots and flatpack build recepe

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -4,12 +4,28 @@
 #
 
 builddir="../build/flatpack"
-manifest="eu.quelltext.open_chakra_toning.yml"
+id="eu.quelltext.open_chakra_toning"
+manifest="$id.yml"
+repo="repo"
+reponame="open_chakra_toning_repo"
 
 cd "`dirname \"$0\"`"
 
 flatpak-builder --force-clean "$builddir" "$manifest"
-flatpak-builder --user --install --force-clean "$builddir" "$manifest"
+## The following commands installs the app directly
+#flatpak-builder --user --install --force-clean "$builddir" "$manifest"
+
+## The Following commands install the app through a repository
+flatpak-builder --repo=repo --force-clean "$builddir" "$manifest"
+flatpak --user remote-add --no-gpg-verify "$reponame" "$repo"
+flatpak --user install "$reponame" "$id"
+
+## Linter
+## see https://docs.flathub.org/docs/for-app-authors/submission/#before-submission
+
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest "$manifest"
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder "$repo" "$repo"
 
 echo "You can now run the app:"
-echo "  flatpak run eu.quelltext.open_chakra_toning"
+echo
+echo "flatpak run $id"
