3f6a28d2c4b18f0c75a71b069ae55af9e247d35c
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-03-25 00:04:07 +0000

    echo build steps flatpak

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -19,7 +19,7 @@
 # Build the flatpack.
 #
 
-builddir="../build/flatpack"
+builddir="../build/flatpak"
 id="eu.quelltext.open_chakra_toning"
 manifest="$id.yml"
 repo="repo"
@@ -29,38 +29,38 @@ builder="flatpak run org.flatpak.Builder"
 set -e
 cd "`dirname \"$0\"`"
 
-## Install the build environment
+echo "Install the build environment"
 ## see https://docs.flatpak.org/en/latest/first-build.html
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo || flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub org.flatpak.Builder
 flatpak install -y flathub org.freedesktop.Platform//23.08
 flatpak install -y flathub org.freedesktop.Sdk//23.08
 
-## Uninstall the currently installed application
+echo "Uninstall the currently installed application"
 flatpak uninstall -y eu.quelltext.open_chakra_toning || true
 
 $builder --force-clean "$builddir" "$manifest"
-## (1) The following commands installs the app directly
+echo "(1) The following commands installs the app directly"
 #flatpak-builder --user --install --force-clean "$builddir" "$manifest"
 
-## (2) The following commands install the app according to the submission section
+echo "(2) The following commands install the app according to the submission section"
 ## see https://docs.flathub.org/docs/for-app-authors/submission#before-submission
-flatpak run org.flatpak.Builder --force-clean --user --install --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ "$builddir" "$manifest"
+$builder --force-clean --user --install --ccache --repo="$repo" --mirror-screenshots-url=https://dl.flathub.org/media/ "$builddir" "$manifest"
 
-## (3) The Following commands install the app through a repository
-#$builder --repo=repo --force-clean "$builddir" "$manifest"
+echo "(3) The Following commands install the app through a repository"
+#$builder --repo="$repo" --force-clean "$builddir" "$manifest"
 #flatpak remote-delete --force "$reponame"
 #flatpak --user remote-add --no-gpg-verify "$reponame" "$repo"
 #flatpak --user install -y "$reponame" "$id"
 
-## Linter
+echo "Linter"
 ## see https://docs.flathub.org/docs/for-app-authors/submission/#before-submission
 
 flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest "$manifest"
 flatpak run --command=flatpak-builder-lint org.flatpak.Builder "$repo" "$repo"
 
 ## flatpak file
-flatpak build-bundle repo "../build/$id.flatpak" "$id"
+flatpak build-bundle "$repo" "../build/$id.flatpak" "$id"
 
 echo "You can now run the app:"
 echo
