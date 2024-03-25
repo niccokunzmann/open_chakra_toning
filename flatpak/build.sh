ac49b19510b36923c651f3b8aacc51d9862d1858
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-03-25 00:43:33 +0000

    skip linter errors

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -25,19 +25,21 @@ manifest="$id.yml"
 repo="repo"
 reponame="open_chakra_toning_repo"
 builder="flatpak run org.flatpak.Builder"
+SUDO=""
 
 set -e
 cd "`dirname \"$0\"`"
 
 echo "Install the build environment"
 ## see https://docs.flatpak.org/en/latest/first-build.html
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo || sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub org.flatpak.Builder
-flatpak install -y flathub org.freedesktop.Platform//23.08
-flatpak install -y flathub org.freedesktop.Sdk//23.08
+if ! flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo; then
+    sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    SUDO=sudo
+fi
+$SUDO flatpak install -y flathub org.flatpak.Builder flathub org.freedesktop.Platform//23.08 flathub org.freedesktop.Sdk//23.08
 
 echo "Uninstall the currently installed application"
-flatpak uninstall -y eu.quelltext.open_chakra_toning || true
+$SUDO flatpak uninstall -y eu.quelltext.open_chakra_toning || true
 
 #$builder --force-clean "$builddir" "$manifest"
 echo "(1) The following commands installs the app directly"
@@ -45,7 +47,7 @@ echo "(1) The following commands installs the app directly"
 
 echo "(2) The following commands install the app according to the submission section"
 ## see https://docs.flathub.org/docs/for-app-authors/submission#before-submission
-$builder --force-clean --user --install --ccache --repo="$repo" --mirror-screenshots-url=https://dl.flathub.org/media/ "$builddir" "$manifest"
+$SUDO $builder --force-clean --user --install --ccache --repo="$repo" --mirror-screenshots-url=https://dl.flathub.org/media/ "$builddir" "$manifest"
 
 echo "(3) The Following commands install the app through a repository"
 #$builder --repo="$repo" --force-clean "$builddir" "$manifest"
@@ -53,11 +55,16 @@ echo "(3) The Following commands install the app through a repository"
 #flatpak --user remote-add --no-gpg-verify "$reponame" "$repo"
 #flatpak --user install -y "$reponame" "$id"
 
-echo "Linter"
+echo "Linter 1"
 ## see https://docs.flathub.org/docs/for-app-authors/submission/#before-submission
-
 flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest "$manifest"
-flatpak run --command=flatpak-builder-lint org.flatpak.Builder "$repo" "$repo"
+echo "Linter 2"
+errors="../build/flatpak-lint.error.txt"
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder --exceptions "$repo" "$repo" | tee "$errors"
+if [ "`cat \"$errors\"`" != "`cat flatpak-lint.error.txt`" ]; then
+  echo "Linter error!"
+  exit 1
+fi
 
 ## flatpak file
 flatpak build-bundle "$repo" "../build/$id.flatpak" "$id"
