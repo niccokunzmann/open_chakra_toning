11b10a5f0723645a323d732a6585657c4ca8df7c
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-03-25 01:11:12 +0000

    outsource linter

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -35,6 +35,8 @@ echo "Install the build environment"
 if ! flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo; then
     sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
     SUDO=sudo
+else
+    USER="--user"
 fi
 $SUDO flatpak install -y flathub org.flatpak.Builder flathub org.freedesktop.Platform//23.08 flathub org.freedesktop.Sdk//23.08
 
@@ -43,28 +45,19 @@ $SUDO flatpak uninstall -y eu.quelltext.open_chakra_toning || true
 
 #$builder --force-clean "$builddir" "$manifest"
 echo "(1) The following commands installs the app directly"
-$builder --user --install --force-clean "$builddir" "$manifest" || true
+#$builder --user --install --force-clean "$builddir" "$manifest" || true
 
 echo "(2) The following commands install the app according to the submission section"
 ## see https://docs.flathub.org/docs/for-app-authors/submission#before-submission
-$builder --force-clean --user --install --ccache --repo="$repo" --mirror-screenshots-url=https://dl.flathub.org/media/ "$builddir" "$manifest" || true
+$builder --force-clean --install --user --ccache --repo="$repo" --mirror-screenshots-url=https://dl.flathub.org/media/ "$builddir" "$manifest"
 
 echo "(3) The Following commands install the app through a repository"
-$builder --repo="$repo" --force-clean "$builddir" "$manifest" || true
-flatpak remote-delete --force "$reponame" || true
-flatpak --user remote-add --no-gpg-verify "$reponame" "$repo"
-flatpak --user install -y "$reponame" "$id"
+#$builder --repo="$repo" --force-clean "$builddir" "$manifest" || true
+#flatpak remote-delete --force "$reponame" || true
+#flatpak --user remote-add --no-gpg-verify "$reponame" "$repo"
+#flatpak --user install -y "$reponame" "$id"
 
-echo "Linter 1"
-## see https://docs.flathub.org/docs/for-app-authors/submission/#before-submission
-flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest "$manifest"
-echo "Linter 2"
-errors="../build/flatpak-lint.error.txt"
-flatpak run --command=flatpak-builder-lint org.flatpak.Builder --exceptions "$repo" "$repo" | tee "$errors"
-if [ "`cat \"$errors\"`" != "`cat flatpak-lint.error.txt`" ]; then
-  echo "Linter error!"
-  exit 1
-fi
+./lint.sh
 
 ## flatpak file
 flatpak build-bundle "$repo" "../build/$id.flatpak" "$id"
